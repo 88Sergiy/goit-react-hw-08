000fd67483b053df1925b18da42b22203244e055
88Sergiy <152657236+88Sergiy@users.noreply.github.com>
2024-08-27 21:04:17 +0300

    commit-11

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,41 +1,46 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { register, logIn, logOut, refreshUser } from "./operations";
 
-axios.defaults.baseURL = "https://connections-api.goit.global/";
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: {
+      name: null,
+      email: null,
+    },
+    token: null,
+    isLoggedIn: false,
+    isRefreshing: false,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.fulfilled, (state) => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state) => {
+        state.isRefreshing = false;
+      });
+  },
+});
 
-export const fetchContacts = createAsyncThunk(
-  "contacts/fetchAll",
-  async (_, thunkAPI) => {
-    try {
-      const response = await axios.get("/contacts");
-
-      return response.data;
-    } catch (e) {
-      console.log("err");
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
-
-export const addContact = createAsyncThunk(
-  "contacts/addContacts",
-  async (body, thunkAPI) => {
-    try {
-      const response = await axios.post("/contacts", body);
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
-export const deleteContact = createAsyncThunk(
-  "contacts/deleteContact",
-  async (taskId, thunkAPI) => {
-    try {
-      const response = await axios.delete(`/contacts/${taskId}`);
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
+export const authReducer = authSlice.reducer;
